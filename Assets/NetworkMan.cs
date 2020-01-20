435a6512782cd506f88885b94df266fcea5c86a5
ken-kun <kevin.ly96@live.ca>
2020-01-20 15:24:37 -0500

    Update NetworkMan.cs

--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -13,11 +13,11 @@ public class NetworkMan : MonoBehaviour
     void Start()
     {
         udp = new UdpClient();
-        
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
+
+        udp.Connect("ec2-54-147-162-251.compute-1.amazonaws.com", 12345);
 
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
-      
+
         udp.Send(sendBytes, sendBytes.Length);
 
         udp.BeginReceive(new AsyncCallback(OnReceived), udp);
@@ -25,64 +25,77 @@ public class NetworkMan : MonoBehaviour
         InvokeRepeating("HeartBeat", 1, 1);
     }
 
-    void OnDestroy(){
+    void OnDestroy()
+    {
         udp.Dispose();
     }
 
 
-    public enum commands{
+    public enum commands
+    {
         NEW_CLIENT,
         UPDATE
     };
-    
+
     [Serializable]
-    public class Message{
+    public class Message
+    {
         public commands cmd;
     }
-    
+
     [Serializable]
-    public class Player{
+    public class Player
+    {
         public string id;
-        public struct receivedColor{
+        [Serializable]
+        public struct receivedColor
+        {
             public float R;
             public float G;
             public float B;
         }
-        public receivedColor color;        
+        public receivedColor color;
     }
 
     [Serializable]
-    public class NewPlayer{
-        
+    public class NewPlayer
+    {
+
     }
 
     [Serializable]
-    public class GameState{
+    public class GameState
+    {
         public Player[] players;
     }
 
     public Message latestMessage;
     public GameState lastestGameState;
-    void OnReceived(IAsyncResult result){
+    void OnReceived(IAsyncResult result)
+    {
         // this is what had been passed into BeginReceive as the second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
-        
+
         // points towards whoever had sent the message:
         IPEndPoint source = new IPEndPoint(0, 0);
 
         // get the actual message and fill out the source:
         byte[] message = socket.EndReceive(result, ref source);
-        
+
         // do what you'd like with `message` here:
         string returnData = Encoding.ASCII.GetString(message);
         Debug.Log("Got this: " + returnData);
-        
+
         latestMessage = JsonUtility.FromJson<Message>(returnData);
-        try{
-            switch(latestMessage.cmd){
+        try
+        {
+            switch (latestMessage.cmd)
+            {
                 case commands.NEW_CLIENT:
+                    GotNewPlayer = true;
                     break;
                 case commands.UPDATE:
+                    currentGameState = true;
                     lastestGameState = JsonUtility.FromJson<GameState>(returnData);
                     break;
                 default:
@@ -90,32 +103,61 @@ public class NetworkMan : MonoBehaviour
                     break;
             }
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             Debug.Log(e.ToString());
         }
-        
+
         // schedule the next receive operation once reading is done:
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
+    GameObject cube;
+    bool GotNewPlayer;
+    bool currentGameState;
+    void SpawnPlayers()
+    {
+        if (GotNewPlayer)
+        {
 
-    void SpawnPlayers(){
+            cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(0, 0.5f, 0);
+
+        }
+        GotNewPlayer = false;
+
+
+
+
+
+
 
     }
 
-    void UpdatePlayers(){
+    void UpdatePlayers()
+    {
+        if (currentGameState)
+        {
+            var cubeRenderer = cube.GetComponent<Renderer>();
+            cubeRenderer.material.color = new Color(lastestGameState.players[0].color.R, lastestGameState.players[0].color.G, lastestGameState.players[0].color.B);
+
+        }
+        currentGameState = false;
 
     }
 
-    void DestroyPlayers(){
+    void DestroyPlayers()
+    {
 
     }
-    
-    void HeartBeat(){
+
+    void HeartBeat()
+    {
         Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
         udp.Send(sendBytes, sendBytes.Length);
     }
 
-    void Update(){
+    void Update()
+    {
         SpawnPlayers();
         UpdatePlayers();
         DestroyPlayers();
